dfc1e4017e31d8236dc74b810e2de5b12eb31908
Kurt <Kurtgw777@gmail.com>
2018-02-03 09:55:11 -0500

    Changed some stuff for the new drive train

--- PowerUp/src/org/usfirst/frc/team548/robot/Constants.java
+++ PowerUp/src/org/usfirst/frc/team548/robot/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static final int DT_TALON_RIGHTBACK = 2;
 	public static final int DT_TALON_LEFTFRONT = 13;
 	public static final int DT_TALON_LEFTBACK = 14;
-	public static final int DT_SOLENOID_SHIFTER = 0;
+	public static final int DT_SOLENOID_SHIFTER = 4;
 	public static final boolean DT_SOLENOID_BOOLEAN = true;
 }
